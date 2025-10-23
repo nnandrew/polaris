e486484492115024e9b839ece69668775fed9f81
Andrew Nguyen <andrewvnguyen@utexas.edu>
2025-10-23 11:26:56 -0500

    lighthouse: more influx patches...

--- lighthouse/influxdb/entrypoint.sh
+++ lighthouse/influxdb/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# Create admin token file if it doesn't exist
+# Create admin token file (exp: 2125/01/01) if it doesn't exist
 if [ ! -f /var/lib/influxdb3/tokens/admin-token.json ]; then
     mkdir -p /var/lib/influxdb3/tokens
     cat > /var/lib/influxdb3/tokens/admin-token.json <<EOF
 {
-  "token": "apiv3_$INFLUX_TOKEN",
+  "token": "$INFLUX_TOKEN",
   "name": "main-token",
-  "expiry_millis": 1756400061529
+  "expiry_millis": 4891384800000 
 }
 EOF
 fi
@@ -17,5 +17,4 @@ exec influxdb3 serve \
     --node-id=node0 \
     --object-store=file \
     --data-dir=/var/lib/influxdb3/data \
-    --plugin-dir=/var/lib/influxdb3/plugins \
     --admin-token-file=/var/lib/influxdb3/tokens/admin-token.json
